533116bd35c6de8e785bd8b3070686dde524c5bd
Fabio Calefato <fcalefato@gmail.com>
2021-11-16 15:08:25 +0100

    Add MODELS_DIR

--- config/config.py
+++ config/config.py
@@ -12,10 +12,12 @@ BASE_DIR = Path(__file__).parent.parent.absolute()
 CONFIG_DIR = Path(BASE_DIR, "config")
 LOGS_DIR = Path(BASE_DIR, "logs")
 DATA_DIR = Path(BASE_DIR, "data")
+MODELS_DIR = Path(BASE_DIR, "models")
 
 # Create dirs
 LOGS_DIR.mkdir(parents=True, exist_ok=True)
 DATA_DIR.mkdir(parents=True, exist_ok=True)
+MODELS_DIR.mkdir(parents=True, exist_ok=True)
 
 # Use config file to initialize logger
 logging.config.fileConfig(Path(CONFIG_DIR, "logging.config"))
